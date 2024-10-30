53c1baaef24075377d734310e16c6e19fcf55801
Gracy222 <gbamidele23@gmail.com>
2024-10-30 06:42:44 +0100

    Task_5

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,48 +1,59 @@
+// config/webpack.config.js
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
-	entry: './src/index.js',
+	entry: path.resolve(__dirname, '../src/index.js'),
 	output: {
+		path: path.resolve(__dirname, '../dist'),
 		filename: 'bundle.js',
-		path: path.resolve('./dist')
+	},
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, '../dist')
+		},
+		hot: true,
+		port: 8080, // optional: specify port if needed
 	},
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
+				use: ['style-loader', 'css-loader'],
 			},
 			{
 				test: /\.(png|jpe?g|gif)$/i,
 				use: [
-					'file-loader',
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'images/',
+						},
+					},
 					{
 						loader: 'image-webpack-loader',
 						options: {
 							bypassOnDebug: true,
-							disable: true
+							disable: true,
 						},
 					},
 				],
 			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react']
-					}
-				}
-			}
-		]
+		],
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, '../dist/index.html'),
+		}),
+	],
 	devtool: 'inline-source-map',
-	devServer: {
-		static: path.resolve('./dist'),
-		compress: true,
-		port: 8564,
-	},
 };
